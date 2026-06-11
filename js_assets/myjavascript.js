e6d7c6980bc7a53c5ee4517c8d608a3e2f0bcfa5
Prosperity13-tech <st10525182@rcconnect.edu.za>
2026-06-11 13:03:04 +0200

    Add files via upload

---
+++ js_assets/myjavascript.js
@@ -0,0 +1,7 @@
+lightbox.option({
+    'resizeDuration' :200,  // image resizing animation 0.2 seconds
+    'wrapAround' :true,    // last image link back to the first page
+    'fadeDuration' :300,    // fade effect takes 0.3 seconds
+
+
+})
